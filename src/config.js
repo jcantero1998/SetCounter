79666470a55fe0516fc49e03e0cc4c2efdd1b244
unknown <jcantero1998@gmail.com>
2023-01-04 20:10:17 +0100

    Variable del puerto de la bbdd

--- src/config.js
+++ src/config.js
@@ -6,3 +6,5 @@ export const DB_HOST = process.env.DB_HOST || "eu-central.connect.psdb.cloud";
 export const DB_USER = process.env.DB_USER || "qrdm30ho2v7or8uatjeb";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "pscale_pw_9tT5FD1T0dKP7eIm0GALJ6L9BzYsTqWZ9kLgdL1G9Lt";
 export const DB_DATABASE = process.env.DB_DATABASE || "setcounterdb";
+export const DB_PORT = process.env.DB_DATABASE || 3306;
+
